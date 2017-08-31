f4efe1b721f9211c002d5f268a1be7d216effaa2
Anthony Shaw <anthonyshaw@apache.org>
2017-08-31 11:47:28 +1000

    timing for characters

--- typer.py
+++ typer.py
@@ -1,3 +1,11 @@
+'''
+    PyTyper
+
+    usage: python typer.py <path to source file>
+
+    Requires: pyautogui and requirements for OS.
+'''
+
 from __future__ import print_function
 
 import sys
@@ -6,14 +14,36 @@ import time
 import random
 
 
+class Speeds:
+    INSTANT = 0.0
+    FAST = 0.05
+    NORMAL = 0.1
+    SLOW = 0.2
+
+
+CHAR_MAP = {
+    ' ': Speeds.INSTANT,
+    '(': Speeds.SLOW,
+    ')': Speeds.SLOW
+}
+
+
 def calc_pause(character=None):
-    return random.uniform(0.1, 0.2)
+    try:
+        return CHAR_MAP[character]
+    except KeyError:
+        return random.uniform(0.05, 0.1)
 
 print("Before the timer finishes, set your mouse cursor to the IDE")
+
 for i in range(1, 5):
     time.sleep(1)
     print("Starting in {0}".format(5-i))
 
 with open(sys.argv[1], 'r') as input_file:
-    for line in input_file:
-        pyautogui.typewrite(line, interval=calc_pause)
+    for line in input_file.readlines():
+        for c in line:
+            time.sleep(calc_pause(c))
+            if c == '\n': #  Helps with autocomplete in IDE's
+                pyautogui.keyDown('escape')
+            pyautogui.press(c)
